f9db5d0682b680363fafe4738c377a4914d08378
lyinggame4356 <lying.game4356@tutanota.com>
2024-04-20 14:09:38 +0300

    fix bug

---
+++ Beer Company/config.php
@@ -0,0 +1,6 @@
+<?php
+define('BEER_TYPE', array('Doppelbock','Golden','Spiced Beers','India Pale Ale','Gose','Scottish Ale','Witbier'));
+define('DB_HOST','localhost');
+define('DB_NAME','weeeklybeer');
+define('DB_PASS',"");
+define('DB_ROOT','root');
